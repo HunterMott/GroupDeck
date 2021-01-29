52cffb34960cd10b7e5a96380ee8309720d013f6
Hunter <huntermott42@gmail.com>
2021-01-28 20:57:07 -0700

    new dev branch

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :events
   has_secure_password
-
+# auth 
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
