f585824de686d6e32b16f421727591f6ed34fbeb
Hunter <huntermott42@gmail.com>
2021-02-01 19:29:32 -0700

    fixed seed again lulw

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,10 @@ Event.destroy_all
 Interest.destroy_all
 User.destroy_all
 
-User.create!({ username: 'hunter', email: 'hunter@hunter.com', password: '1234567'})
+@user = User.create!({ username: 'hunter', email: 'hunter@hunter.com', password: '1234567'})
 
 
-Interest.create!([{name: 'Tech'}, {name: 'Outdoors & Adventure'}, {name: 'Family'}, {name: 'Health & Wellness'}, {name: 'Sports & Fitness'}, {name: 'Learning'}, {name: 'Photography'}, {name: 'Food & Drink'}, {name: 'Writing'}, {name: 'Language & Cultue'}, {name: 'Music'}, {name: 'Movements'}, {name: 'LGBTQ'}, {name: 'Film'}, {name: 'Sci-Fi & Games'}, {name: 'Beliefs'}, {name: 'Arts'}, {name: 'Book Clubs'}, {name: 'Dance'}, {name: 'Pets'}, {name: 'Hobbies & Crafts'}, {name: 'Fashion & Beauty'}, {name: 'Social'}, {name: 'Career & Business'}])
+@interest = Interest.create!([{name: 'Tech'}, {name: 'Outdoors & Adventure'}, {name: 'Family'}, {name: 'Health & Wellness'}, {name: 'Sports & Fitness'}, {name: 'Learning'}, {name: 'Photography'}, {name: 'Food & Drink'}, {name: 'Writing'}, {name: 'Language & Cultue'}, {name: 'Music'}, {name: 'Movements'}, {name: 'LGBTQ'}, {name: 'Film'}, {name: 'Sci-Fi & Games'}, {name: 'Beliefs'}, {name: 'Arts'}, {name: 'Book Clubs'}, {name: 'Dance'}, {name: 'Pets'}, {name: 'Hobbies & Crafts'}, {name: 'Fashion & Beauty'}, {name: 'Social'}, {name: 'Career & Business'}])
 
 
 Event.create!({
@@ -21,6 +21,6 @@ Event.create!({
   description: 'Come learn how to interview properly like a real adult and get the job you want now!',
   date: 'Feb 8th, 2021',
   img_url: 'https://images.unsplash.com/photo-1553877522-43269d4ea984?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
-  user_id: 1,
-  interest_id: 1
+  user: @user,
+  interest: @interest
 })
